d3169290acb9d033c18e4f18f49c8d3692c8aeac
l-zeng <125086973+l-zeng@users.noreply.github.com>
2023-02-24 15:54:19 -0500

    Create sketch.js

---
+++ John Whitney/sketch.js
@@ -0,0 +1,90 @@
+function setup() {
+  createCanvas(667, 500);
+  noStroke();
+}
+
+function draw() {
+  background(0);
+  const time = Date.now()/500;
+//1--------------------------------------------------
+  fill(43, 54, 153, 255);
+  beginShape();
+  vertex(0,500);
+  for (let i = 0; i < 800; i += 1) {
+    const y1 = 220 + 135 * cos(time/1.65 + i * 0.003);
+    vertex(i, y1);
+  }
+  vertex(667,500);
+  endShape(true);
+//2--------------------------------------------------
+  fill(58, 123, 249, 255);
+  beginShape();
+  vertex(0,500);
+  for (let i = 0; i < 800; i += 1) {
+    const y1 = 190 + 30 * cos(time/1.7 + i * 0.005);
+    vertex(i, y1);
+  }
+  vertex(667,500);
+  endShape(true);
+//3--------------------------------------------------
+  fill(112,124,242,204);
+  beginShape();
+  vertex(0,500);
+  for (let i = 0; i < 800; i += 1) {
+    const y1 = 93 + 87 * sin(time/1.5 + i * 0.003);
+    vertex(i, y1);
+  }
+  vertex(667,500);
+  endShape(true);
+//4--------------------------------------------------
+  fill(185, 190, 248, 127.5);
+  beginShape();
+  vertex(0,500);
+  for (let i = 0; i < 800; i += 1) {
+    const y1 = 323 + 87 * sin(time/1.5 + i * 0.003);
+    vertex(i, y1);
+  }
+  vertex(667,500);
+  endShape(true);
+//5--------------------------------------------------
+  fill(165, 177, 252, 127.5);
+  beginShape();
+  vertex(0,500);
+  for (let i = 0; i < 800; i += 1) {
+    const y1 = 320 + 28 * cos(time/1.5 + i * 0.005);
+    vertex(i, y1);
+  }
+  vertex(667,500);
+  endShape(true);
+//6--------------------------------------------------
+  fill(77, 93, 200, 178.5);
+  push();
+  rotate(PI/-0.246);
+  beginShape();
+  vertex(-800,600);
+  for (let i = 0; i < 800; i += 1) {
+    const y1 =520 + 35 * cos(time/0.6 + i * 0.0075);
+    vertex(i, y1);
+  }
+  vertex(667,800);
+  endShape(true);
+  pop();
+//7--------------------------------------------------
+  fill(97, 102, 219, 255);
+  push();
+  translate(-10, 0);
+  rotate(PI/-0.2485);
+  beginShape();
+  vertex(-100,600);
+  for (let i = 0; i < 800; i += 1) {
+    const y1 =535 + 20 * sin(time/0.6 + i * 0.0055);
+    vertex(i, y1);
+  }
+  vertex(667,800);
+  endShape(true);
+  pop();
+}
+
+
+
+
